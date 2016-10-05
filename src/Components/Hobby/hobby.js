2273c3765c897ad2a231f4aee2680150cb5c28a3
John Kim <joimkim@gmail.com>
2016-10-04 21:54:48 -0700

    first round done

--- src/Components/Hobby/hobby.js
+++ src/Components/Hobby/hobby.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import './styles.css';
 
 const Hobby = React.createClass({
 	render() {
 		return (
 			<div className='col-12'>
 				<h3 className="Title">Hobbies</h3>
-				<div>
+				<div className="Hobbies">
+					<p>
+			      <FontAwesome
+			        className='super-crazy-colors'
+			        name='camera-retro'
+			        size='1x'
+			      />
+					</p>
+					<p>
+			      <FontAwesome
+			        className='super-crazy-colors'
+			        name='beer'
+			        size='1x'
+			      />
+					</p>
+					<p>
+			      <FontAwesome
+			        className='super-crazy-colors'
+			        name='music'
+			        size='1x'
+			      />
+					</p>
+					<p>
+			      <FontAwesome
+			        className='super-crazy-colors'
+			        name='gamepad'
+			        size='1x'
+			      />
+					</p>
+					<p>
+			      <FontAwesome
+			        className='super-crazy-colors'
+			        name='pencil'
+			        size='1x'
+			      />
+					</p>						
 				</div>
 			</div>
 		);

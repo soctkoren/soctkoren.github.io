27472871bb9c023c9831185f1736c4543c8000c9
John Kim <joimkim@gmail.com>
2016-10-04 20:45:18 -0700

    work done

--- src/Components/Skill/skill.js
+++ src/Components/Skill/skill.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import logo from '../../logo.svg';
+import railslogo from '../../../public/rails.png';
+import sinatra from '../../../public/sinatra.png';
+import ruby from '../../../public/ruby.png';
+import javascript from '../../../public/javascript.png';
 import '../../App.css';
+import './styles.css';
 
 const Skill = React.createClass({
 	render() {
 		return (
 			<div className='col-6'>
 				<h3 className="Title">Skills</h3>
-				<div>
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>React</p>
+				<div className='SkillsContainer'>
+					<div className='Skills'>
+						<img src={ruby} className="ruby" alt="ruby"/>
+						<p>ruby</p>
+					</div>
+					<div className='Skills'>
+						<img src={sinatra} className="sinatra" alt="sinatra"/>
+						<p>Sinatra</p>
+					</div>
+					<div className='Skills'>
+						<img src={logo} className="App-logo" alt="logo" />
+						<p>React</p>
+					</div>
+					<div className='Skills'>
+						<img src={railslogo} className="railsLogo" alt="rails"/>
+						<p>Rails</p>
+					</div>
+					<div className='Skills'>
+						<img src={javascript} className="javascript" alt="javascript"/>
+						<p>JS</p>
+					</div>
 				</div>
 			</div>
 		)
--- src/Components/WorkExperience/WorkExperience.js
+++ src/Components/WorkExperience/WorkExperience.js
@@ -1,12 +1,57 @@
 import React from 'react';
+import d from '../../resume.json';
+import './styles.css'
 
 var WorkExperience = React.createClass({
+	getInitialState: function() {
+		return ({
+			work: [],
+			highlight: []
+		})
+	},
+
+	componentWillMount: function() {
+		
+		var works = [];
+		d.work.forEach(function(w) {
+			var theWork = (
+				<div>
+					<div className="WorkHeader">
+						<div className="col-5 WorkCompany">
+							<h3>{w.company}</h3>
+						</div>
+						<div className="WorkPosition">
+							<p>{w.position}</p>
+							<p>{w.startDate} - {w.endDate}</p>
+						</div>
+					</div>
+					<div>
+						<ul>
+							{w.highlights.map(function(high) {
+								return <li className="green">{high}</li>
+							})}
+						</ul>
+					</div>
+				</div>
+			)
+			works.push(theWork);
+
+		});
+
+
+		this.setState({
+			work: works,
+		});
+	},
+
 	render () {
 		return (
 			<div className="col-6">
-				<h3>Work Experience</h3>
+				<h3 className="Title">Work Experience</h3>
 				<div>
-					all work exp
+					<ul className="experiences">
+						{this.state.work}
+					</ul>
 				</div>
 			</div>
 		);
